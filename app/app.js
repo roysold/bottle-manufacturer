127f259c5d7865fc89fb23b82b4edf97315d85bc
Roy <roysold@gmail.com>
2018-01-30 20:48:34 +0200

    feat(bottles router): WIP: get query params and start manipulating requested data.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bottlesRouter = require("../routers/bottlesRouter.js");
-
+ 
 let app = express();
 
 app.use("/api/v1/bottles", bottlesRouter);
--- routers/bottlesRouter.js
+++ routers/bottlesRouter.js
@@ -5,7 +5,12 @@ let { body, validationResult } = require('express-validator/check');
 let router = express.Router();
 let bottles = require("../data/bottles.json");
 
-bottles = bottles.map(bottle => addLinksToBottle(bottle));
+bottles = bottles.map(bottle => addLinksToBottle(
+    bottle,
+    [
+        ["self", concatURLs("/api/v1/bottles", bottle.id)]
+    ]
+));
 
 router.use(bodyParser.json());
 
@@ -16,9 +21,51 @@ router.use(function (err, req, res, next) {
     }
 });
 
+//sort=-manufactorer,+model
+// fields=manufacturer,model,id,color
+// offset=10&limit=5
+// { sort: '-manufactorer, model',
+//   fields: 'manufacturer,model,id,color',
+//   offset: '10',
+//   limit: '5' }
+
 router.get("/", (req, res) => {
+    console.log(req.query);
+    let bottlesToSend = [];
+    let { offset, limit, fields, sort } = req.query;
+
+    offset = (offset === undefined) ? 0 : parseInt(offset);
+    limit = (limit > bottles.length - offset) ? bottles.length - offset : parseInt(limit);
+    fields = (fields === undefined) ? Object.keys(bottles[0]) : fields.split(",");
+    sort = (sort === undefined) ? [] : sort.split(",");
+
+    let bottlesToSort = sortList(bottles, sort);
+
+    //TODO
+    for (let index = offset; index < offset + limit; index++) {
+        bottlesToSend.push({});
+    }
+
+    bottlesToSend = bottles.map(bottle => {
+        let bottleToReturn = {};
+
+    })
+
     res.json(bottles);
-})
+});
+
+//TODO
+function sortList(list, sortFields) {
+    let sortedList = list.slice();
+
+    sortFields.forEach(field => {
+        if (field.startsWith("+")) {
+            sortedList.sort((item1, item2) => {
+
+            })
+        }
+    })
+}
 
 router.param("id", (req, res, next) => {
     res.locals.indexOfBottleByID =
@@ -40,6 +87,7 @@ router.delete("/:id", (req, res) => {
         .send("Bottle deleted.");
 });
 
+// TODO validate that we don't get any extra unwanted properties
 const bottleValidations = [
     body(["id", "orderID", "factoryID"])
         .isAlphanumeric()
@@ -49,6 +97,7 @@ const bottleValidations = [
         .withMessage('Field is required and must be in ISO8601 format.')
 ];
 
+// TODO change properties or delete and add new object each time?
 router.put("/", bottleValidations,
     (req, res) => {
         const errors = validationResult(req);
@@ -57,7 +106,14 @@ router.put("/", bottleValidations,
             let indexOfBottleToModify = getIndexOfBottleByID(req.body.id);
 
             if (indexOfBottleToModify !== -1) {
-                bottles.splice(indexOfBottleToModify, 1, req.body);
+                bottles.splice(indexOfBottleToModify, 1,
+                    addLinksToBottle(
+                        req.body,
+                        [
+                            ["self", concatURLs(req.originalUrl, req.body.id)]
+                        ]
+                    )
+                );
 
                 res.send();
             } else {
@@ -69,22 +125,31 @@ router.put("/", bottleValidations,
         }
     });
 
+function concatURLs(url1, url2) {
+    return `${url1.replace(/\/*$/, "/")}${url2}`
+}
+
 router.post("/", bottleValidations,
     (req, res) => {
         const errors = validationResult(req);
 
         if (errors.isEmpty()) {
+
             if (getIndexOfBottleByID(req.body.id) !== -1) {
                 res.status(httpStatusCodes.CONFLICT)
                     .json(
                     {
-                        error: "A bottle with the ID '"
-                        + req.body.id
-                        + "' already exists."
+                        error: `A bottle with the ID '${req.body.id}' already exists.`
                     }
                     )
             } else {
-                bottles.push(req.body);
+                bottles.push(addLinksToBottle(
+                    req.body,
+                    [
+                        ["self", concatURLs(req.originalUrl, req.body.id)]
+                    ]
+                )
+                );
 
                 res.status(httpStatusCodes.CREATED).send();
             }
@@ -102,7 +167,7 @@ function getIndexOfBottleByID(id) {
 function sendBottleIDNotFoundError(id, res) {
     res.json(
         {
-            error: "No bottle with the ID '" + id + "'."
+            error: `No bottle with the ID '${id}'.`
         }
     );
 }
@@ -112,13 +177,20 @@ function sendUnprocessableEntityError(errors, res) {
         .json({ errors: errors.mapped() });
 }
 
-function addLinksToBottle(obj) {
+// param: linkDataToAdd - A list of lists, 
+// where each inner list has the format: [rel, href].
+function addLinksToBottle(obj, linkDataToAdd) {
     let objectToReturn = Object.assign({}, obj);
-    objectToReturn.links = [];
 
-    objectToReturn.links.push({
-        rel: "self",
-        href: "/api/v1/bottles/" + objectToReturn.id
+    if (!obj.hasOwnProperty("links")) {
+        objectToReturn.links = [];
+    }
+
+    linkDataToAdd.forEach(linkData => {
+        objectToReturn.links.push({
+            rel: linkData[0],
+            href: linkData[1]
+        });
     });
 
     return objectToReturn;
